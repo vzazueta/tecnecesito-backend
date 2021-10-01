7eb723ca86f7e0052439b049abb33b50ff1f5ed0
vzazueta21 <victor.zazueta@accedo.tv>
2021-09-30 19:25:49 -0500

    Casi todos los endpoints

--- tecnecesito/handler.js
+++ tecnecesito/handler.js
@@ -53,11 +53,161 @@ app.get("/cliente", async (req, res, next) => {
   }
 });
 
+app.get("/acceso_cliente", async (req, res, next) => {
+  try {
+    let cliente = await query(`SELECT \`cliente.correo\` AND \`cliente.contrasena\` FROM  \`cliente\` `);
+    return res.status(200).json({
+      cliente: cliente,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({
+      error: error
+    });
+  }
+});
+
+app.put("/cliente", async (req, res, next) => {
+  try {
+    let cliente = await query(
+      `UPDATE \`cliente\` SET \`nombre\` = IFNULL('${req.body.nombre}', \`nombre\`), \`correo\` = IFNULL('${req.body.correo}', \`correo\`), \`contrasena\` = IFNULL('${req.body.contrasena}', \`contrasena\`), \`telefono\` = IFNULL('${req.body.telefono}', \`telefono\`) WHERE (\`correo\` = '${req.body.correo}')`
+    );
+    return res.status(200).json({
+      cliente: cliente,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({
+      error: error
+    });
+  }
+});
+
 app.delete("/cliente", async (req, res, next) => {
   try {
     let cliente = await query(`DELETE FROM \`cliente\` WHERE (\`correo\` = '${req.body.correo}')`);
     return res.status(200).json({
       cliente: cliente,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({
+      error: error
+    });
+  }
+});
+
+app.post("/profesionista", async (req, res, next) => {
+  try {
+    let profesionista = await query(
+      `INSERT INTO \`profesionista\` (\`nombre\`, \`correo\`, \`contrasena\`, \`ubicacion\`, \`modalidad\`, \`telefono\`, \`categoria\`) VALUES ('${req.body.nombre}', '${req.body.correo}', '${req.body.contrasena}', '${req.body.ubicacion}', '${req.body.modalidad}', '${req.body.telefono}', '${req.body.categoria}')`
+    );
+    return res.status(200).json({
+      profesionista: profesionista,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({
+      error: error
+    });
+  }
+});
+
+app.get("/profesionista", async (req, res, next) => {
+  try {
+    let profesionista = await query(`SELECT * FROM  \`profesionista\` WHERE (\`correo\` = '${req.body.correo}')`);
+    return res.status(200).json({
+      profesionista: profesionista,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({
+      error: error
+    });
+  }
+});
+
+app.get("/acceso_profesionista", async (req, res, next) => {
+  try {
+    let profesionista = await query(`SELECT \`profesionista.correo\` AND \`profesionista.contrasena\` FROM  \`profesionista\` `);
+    return res.status(200).json({
+      profesionista: profesionista,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({
+      error: error
+    });
+  }
+});
+
+app.put("/profesionista", async (req, res, next) => {
+  try {
+    let profesionista = await query(
+      `UPDATE \`profesionista\` SET \`nombre\` = IFNULL('${req.body.nombre}', \`nombre\`), \`correo\` = IFNULL('${req.body.correo}', \`correo\`), \`contrasena\` = IFNULL('${req.body.contrasena}', \`contrasena\`), \`telefono\` = IFNULL('${req.body.telefono}', \`telefono\`), \`ubicacion\` = IFNULL('${req.body.ubicacion}', \`ubicacion\`), \`modalidad\` = IFNULL('${req.body.modalidad}', \`modalidad\`) WHERE (\`correo\` = '${req.body.correo}')`
+    );
+    return res.status(200).json({
+      profesionista: profesionista,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({
+      error: error
+    });
+  }
+});
+
+app.delete("/profesionista", async (req, res, next) => {
+  try {
+    let profesionista = await query(`DELETE FROM \`profesionista\` WHERE (\`correo\` = '${req.body.correo}')`);
+    return res.status(200).json({
+      profesionista: profesionista,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({
+      error: error
+    });
+  }
+});
+
+app.post("/categoria", async (req, res, next) => {
+  try {
+    let categoria = await query(
+      `INSERT INTO \`categoria\` (\`nombre\`) VALUES ('${req.body.nombre}')`
+    );
+    return res.status(200).json({
+      categoria: categoria,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({
+      error: error
+    });
+  }
+});
+
+app.delete("/categoria", async (req, res, next) => {
+  try {
+    let categoria = await query(`DELETE FROM \`categoria\` WHERE (\`nombre\` = '${req.body.nombre}')`);
+    return res.status(200).json({
+      categoria: categoria,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({
+      error: error
+    });
+  }
+});
+
+app.post("/favorito", async (req, res, next) => {
+  try {
+    let favorito = await query(
+      `INSERT INTO \`favorito\` (\`correo\`) VALUES ('${req.body.nombre}')`
+    );
+    return res.status(200).json({
+      favorito: favorito,
     });
   } catch (error) {
     console.log(error);
